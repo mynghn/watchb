c10a4c3ca18fb6fb336fb37625d767ab54324bec
Myounghoon Lee <l.mynghn@gmail.com>
2022-07-20 01:10:39 +0900

    :sparkles: BE) JWT views
    add jwt related api views & routings

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    token_obtain_pair,
+    token_refresh,
+    token_verify,
+)
 
 from . import views
 
-urlpatterns = [path("", views.SignUpView.as_view(), name="sign-up")]
+urlpatterns = [
+    path("", views.SignUpView.as_view(), name="sign-up"),
+    path("jwt/", token_obtain_pair, name="token-obtain-pair"),
+    path("jwt/refresh/", token_refresh, name="token-refresh"),
+    path("jwt/verify/", token_verify, name="token-verify"),
+]
