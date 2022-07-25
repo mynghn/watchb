6b920110070d91e213efe7add115423d04ccaf69
Myounghoon Lee <l.mynghn@gmail.com>
2022-07-25 09:01:46 +0900

    :coffin: BE) Remove token_verify url entry point
    remove unused token verifying url entry point

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import token_refresh, token_verify
+from rest_framework_simplejwt.views import token_refresh
 
 from . import views
 
@@ -7,5 +7,4 @@ urlpatterns = [
     path("", views.SignUpView.as_view(), name="sign-up"),
     path("jwt/", views.JWTObtainPairView.as_view(), name="token-obtain-pair"),
     path("jwt/refresh/", token_refresh, name="token-refresh"),
-    path("jwt/verify/", token_verify, name="token-verify"),
 ]
