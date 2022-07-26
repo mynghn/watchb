6311b5b07e1356e1748b23ab0455fa0cadd1eede
Myounghoon Lee <l.mynghn@gmail.com>
2022-07-26 19:21:31 +0900

    :sparkles: BE) Update sign up API fields
    set id, date_joined to readonly fields and mark password field as writeonly

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -7,4 +7,6 @@ User = get_user_model()
 class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
+        fields = ["username", "email", "password", "id", "date_joined"]
+        read_only_fields = ["id", "date_joined"]
+        extra_kwargs = {"password": {"write_only": True}}
