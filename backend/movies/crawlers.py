42884ca0f41603486fb8a8a70959b00bf53b6695
Myounghoon Lee <l.mynghn@gmail.com>
2022-09-06 15:35:53 +0900

    :sparkles: BE) Implement CountryCrawler for Korean country names
    - crawl ISO 3166-1 country name and codes from wikipedia

--- backend/movies/crawlers.py
+++ backend/movies/crawlers.py
@@ -1,6 +1,8 @@
 from abc import ABCMeta, abstractmethod
 from typing import Any, Type
 
+import requests
+from bs4 import BeautifulSoup
 from rest_framework.serializers import ModelSerializer
 
 from movies.serializers import MovieRegisterSerializer
@@ -40,3 +42,31 @@ class MovieAPICrawler(metaclass=ABCMeta):
         Total process of fetch -> serialize -> register steps of crawling movies from API
         """
         raise NotImplementedError
+
+
+class CountryCrawler:
+    url = "https://ko.wikipedia.org/wiki/ISO_3166-1"
+    _book = {}
+
+    @classmethod
+    def _set_countries(cls):
+        response = requests.get(cls.url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        rows = soup.table.findChildren("tr")
+        keys = [header.text.rstrip() for header in rows.pop(0).findChildren("th")]
+
+        for row in rows:
+            country = {}
+            for idx, cell in enumerate(row.findChildren("td")):
+                country[keys[idx]] = cell.text.strip().upper()
+            for v in country.values():
+                cls._book[v] = country
+
+    @classmethod
+    def get_country(cls, key: str) -> dict[str, str]:
+        if not cls._book:
+            cls._set_countries()
+
+        return cls._book.get(key.upper(), {})
