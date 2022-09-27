86e156954b22ccaf7a03eb0b7bfd464021d4db9b
Myounghoon Lee <l.mynghn@gmail.com>
2022-09-27 18:04:19 +0900

    :adhesive_bandage: BE) Some updates to custom validators
    - fix subbing pattern for validate_kmdb_text
    - fix only korean pattern to allow 0 occurrence of korean char

--- backend/movies/validators.py
+++ backend/movies/validators.py
@@ -2,13 +2,14 @@ import re
 from typing import Literal
 
 from django.core.validators import RegexValidator
-from pycountry import countries
 from rest_framework.serializers import ValidationError
+
+from movies.crawlers.utils import ISO_3166_1
 
 
 class OnlyKoreanValidator(RegexValidator):
     def __init__(self, allowed: str | re.Pattern = []):
-        super(OnlyKoreanValidator, self).__init__(regex=r"^[가-힣]+$", code="only-korean")
+        super(OnlyKoreanValidator, self).__init__(regex=r"^[가-힣]*$", code="only-korean")
         self.allowed = allowed
 
     def __call__(self, value: str):
@@ -28,14 +29,14 @@ class CountryCodeValidator:
         self.code_type = code_type
 
     def __call__(self, value: str):
-        if not countries.get(**{self.code_type: value}):
+        if not ISO_3166_1.get_country(**{self.code_type: value}):
             raise ValidationError(
-                f"ISO 3166-1 {self.code_type} code should be provided.",
-                code="ISO-3166-1",
+                f"Valid ISO 3166-1 {self.code_type} code should be provided but '{value}' encountered.",
+                code="invalid",
             )
 
 
 def validate_kmdb_text(text: str) -> str:
-    cleansed = re.sub(r"!HS|!HE", "", text)  # invalid characters from KMDb
+    cleansed = re.sub(r"\s?(!HS|!HE)\s?", "", text)  # invalid characters from KMDb
     cleansed = re.sub(r"\s+", " ", cleansed)
     return cleansed.strip()
