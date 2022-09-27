2637e7dfe022df49cdc8ace58215e4717dadec6d
Myounghoon Lee <l.mynghn@gmail.com>
2022-09-27 17:42:30 +0900

    :adhesive_bandage: BE) Update ISO_3166_1 country info class
    - added exceptions (ex. 유고슬라비아)
    - revised search interface to use kwargs

--- backend/movies/crawlers/utils.py
+++ backend/movies/crawlers/utils.py
@@ -5,11 +5,11 @@ from bs4 import BeautifulSoup
 class ISO_3166_1:
     url = "https://ko.wikipedia.org/wiki/ISO_3166-1"
 
-    _book: dict
-    name_key: str
-    numeric_key: str
-    alpha2_key: str
-    alpha3_key: str
+    _book: dict[str, dict[str, dict[str, str]]]
+    name_key = "name"
+    numeric_key = "numeric"
+    alpha_2_key = "alpha_2"
+    alpha_3_key = "alpha_3"
 
     @classmethod
     def _setup(cls):
@@ -18,25 +18,39 @@ class ISO_3166_1:
 
         soup = BeautifulSoup(response.text, "html.parser")
         rows = soup.table.findChildren("tr")
-        keys = [header.text.rstrip() for header in rows.pop(0).findChildren("th")]
-        (
-            cls.name_key,
-            cls.numeric_key,
-            cls.alpha_2_key,
-            cls.alpha_3_key,
-        ) = keys
 
-        cls._book = {}
-        for row in rows:
+        keys = [cls.name_key, cls.numeric_key, cls.alpha_3_key, cls.alpha_2_key]
+
+        cls._book = {k: {} for k in keys}
+        for row in rows[1:]:
             country = {}
             for idx, cell in enumerate(row.findChildren("td")):
                 country[keys[idx]] = cell.text.strip().upper()
-            for v in country.values():
-                cls._book[v] = country
+            for k, v in country.items():
+                cls._book[k][v] = country
+
+        cls._setup_exceptions()
 
     @classmethod
-    def get_country(cls, key: str) -> dict[str, str]:
+    def _setup_exceptions(cls):
+        # 유고슬라비아
+        yugoslavia = {
+            cls.name_key: "유고슬라비아",
+            cls.numeric_key: "891",
+            cls.alpha_2_key: "YU",
+            cls.alpha_3_key: "YUG",
+        }
+        for k, v in yugoslavia.items():
+            cls._book[k][v] = yugoslavia
+
+    @classmethod
+    def get_country(cls, **kwargs) -> dict[str, str]:
+        if len(kwargs) != 1:
+            raise ValueError("Only one search criterion is allowed")
+
         if not getattr(cls, "_book", False):
             cls._setup()
 
-        return cls._book.get(key.upper(), {})
+        criterion, value = list(kwargs.items()).pop()
+
+        return cls._book[criterion].get(value.upper(), {})
