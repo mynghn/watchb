973d2858fbf2b7bb5797961f4a9addcfd1e1815b
Myounghoon Lee <l.mynghn@gmail.com>
2022-10-03 17:22:16 +0900

    :card_file_box: BE) Add has_spoiler field to movie Review model

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -130,6 +130,7 @@ class Review(CreateAndUpdateModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
     comment = models.TextField()
+    has_spoiler = models.BooleanField(default=False)
 
 
 class Wishlist(TimestampModel):
