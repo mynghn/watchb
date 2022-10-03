88a0a7db6de0cc8c1a1060e341e1de4feef87884
Myounghoon Lee <l.mynghn@gmail.com>
2022-10-03 19:30:24 +0900

    :sparkles: BE) Implement custom permission class for none user objects
    - check if request.user is objects author

--- backend/accounts/permissions.py
+++ backend/accounts/permissions.py
@@ -4,3 +4,8 @@ from rest_framework import permissions
 class IsSelfOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, user_obj):
         return user_obj == request.user or request.user.is_superuser
+
+
+class IsAuthorOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user or request.user.is_superuser
