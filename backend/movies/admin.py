e703d48584346df7fd724b324a74bc282dcf95ce
Myounghoon Lee <l.mynghn@gmail.com>
2022-10-09 15:44:41 +0900

    :technologist: BE) Add new movies domain models to admin site

--- backend/movies/admin.py
+++ backend/movies/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from .models import Movie, Person
+from .models import (
+    Blocklist,
+    Movie,
+    Person,
+    PersonLike,
+    Rating,
+    Review,
+    ReviewLike,
+    Wishlist,
+)
 
 admin.site.register(Movie, ModelAdmin)
 admin.site.register(Person, ModelAdmin)
+admin.site.register(Rating, ModelAdmin)
+admin.site.register(Review, ModelAdmin)
+admin.site.register(Wishlist, ModelAdmin)
+admin.site.register(Blocklist, ModelAdmin)
+admin.site.register(ReviewLike, ModelAdmin)
+admin.site.register(PersonLike, ModelAdmin)
